86d604505346c99f865f7b4d97db28f2dfaa2362
Maxime48 <maxime4570@gmail.com>
2022-06-09 01:22:11 +0200

    Correctly handling css,html,images and js

--- src/CustomHandler.java
+++ src/CustomHandler.java
@@ -1,30 +1,57 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class CustomHandler implements HttpHandler {
 
     public void handle(HttpExchange exchange) throws IOException {
         String requestMethod = exchange.getRequestMethod();
-
         String query = exchange.getRequestURI().getQuery();
 
         URI ops = exchange.getRequestURI();
 
-        //reading requested file
-        BufferedReader objReader = new BufferedReader(new FileReader(Server.root+ops.getPath()));
-        String response = objReader.lines().collect(Collectors.joining());
+        //handling extensions and filetype
+        String uri = ops.getPath();
+        String extension = (uri.contains(".")) ? uri.substring(uri.lastIndexOf(".") + 1) : "";
+        if (extension.equals("js")) {
+            extension = "javascript";
+        }
+        String filetype = "text";
+        if (extension.equals("png") || extension.equals("jpg")) {
+            filetype = "image";
+        }
 
-        exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-        exchange.sendResponseHeaders(200, response.getBytes().length);
+        //reading requested file
+        String response;
+        byte[] imageBytes = new byte[0];
+        if (filetype.equals("image")) {
+            imageBytes = Files.readAllBytes(Path.of(Server.root + ops.getPath()));
+            response = new String(imageBytes);
+        } else {
+            BufferedReader BufferedfileReader = new BufferedReader(
+                    new FileReader(Server.root + ops.getPath())
+            );
+            response = BufferedfileReader.lines().collect(Collectors.joining());
+        }
+
+        //Headers
+        exchange.getResponseHeaders().set(
+                "Content-Type", filetype + "/" + extension + ";" + " charset=UTF-8"
+        );
+
+        exchange.sendResponseHeaders(200,
+            (filetype.equals("image")) ? imageBytes.length : response.getBytes().length
+        );
         OutputStream os = exchange.getResponseBody();
-        os.write(response.getBytes());
+        os.write(
+                (filetype.equals("image")) ? imageBytes : response.getBytes()
+        );
         os.close();
     }
 
